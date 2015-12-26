f14c3503fb17f5df20fe90f51e7def3517d60dab
will-e-p <will.e.p@gmail.com>
2015-12-26 11:15:37 -0800

    Added honeycomb triangular meshes

--- src/Meshes.cpp
+++ src/Meshes.cpp
@@ -19,6 +19,11 @@ PolyMesh triUnitSquare(double h)
     return triRectangle(h, 1, 1);
 }
 
+PolyMesh honeycombUnitSquare(double h)
+{
+    return honeycombRectangle(h, 1, 1);
+}
+
 PolyMesh hexRectangle(double h, double width, double height)
 {
     double x, y;
@@ -26,12 +31,12 @@ PolyMesh hexRectangle(double h, double width, double height)
     vector<array<double, 2> > generatingPoints;
     int yi;
 
-    for(x = 0; x <= width; x += h)
+    for(x = 0; x <= width + kEPS; x += h)
     {
         yi = 0;
-        for(y = 0; y <= height; y += h)
+        for(y = 0; y <= height + kEPS; y += h)
         {
-            if (x + 0.5*h*yi < width)
+            if (x + 0.5*h*yi <= width + kEPS)
             {
                 pt[0] = x + 0.5*h*yi;
                 pt[1] = y;
@@ -79,6 +84,35 @@ PolyMesh triRectangle(double h, double width, double height)
             pt[0] = x;
             pt[1] = y;
             generatingPoints.push_back(pt);
+        }
+    }
+    
+    msh.triangulate(generatingPoints);
+    return msh;
+}
+
+
+PolyMesh honeycombRectangle(double h, double width, double height)
+{
+    double x, y;
+    array<double, 2> pt;
+    vector<array<double, 2> > generatingPoints;
+    int yi;
+    PolyMesh msh;
+
+    for(x = 0; x <= width; x += h)
+    {
+        yi = 0;
+        for(y = 0; y <= height; y += h)
+        {
+            if (x + 0.5*h*yi < width)
+            {
+                pt[0] = x + 0.5*h*yi;
+                pt[1] = y;
+                generatingPoints.push_back(pt);
+            }
+            
+            yi = !yi;
         }
     }
     
--- src/Meshes.h
+++ src/Meshes.h
@@ -5,7 +5,9 @@
 PolyMesh quadRectangle(double h, double width, double height);
 PolyMesh hexRectangle(double h, double width, double height);
 PolyMesh triRectangle(double h, double width, double height);
+PolyMesh honeycombRectangle(double h, double width, double height);
 
+PolyMesh honeycombUnitSquare(double h);
 PolyMesh quadUnitSquare(double h);
 PolyMesh hexUnitSquare(double h);
 PolyMesh triUnitSquare(double h);
