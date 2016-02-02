1a8fb807d78a709f4a6cbc1ded1bbbad788518ca
will-e-p <will.e.p@gmail.com>
2016-02-01 16:18:44 -0800

    Added perturbed meshes

--- src/Meshes.cpp
+++ src/Meshes.cpp
@@ -37,9 +37,9 @@ PolyMesh quadRectangle(double h, double width, double height)
     array<double, 2> pt;
     vector<array<double, 2> > generatingPoints;
     
-    for(x = 0.5*h; x <= width; x += h)
+    for(x = 0.5*h; x <= width + kEPS; x += h)
     {
-        for(y = 0.5*h; y <= height; y += h)
+        for(y = 0.5*h; y <= height + kEPS; y += h)
         {
             pt[0] = x;
             pt[1] = y;
@@ -50,24 +50,95 @@ PolyMesh quadRectangle(double h, double width, double height)
     return PolyMesh(generatingPoints, width, height);
 }
 
-PolyMesh triRectangle(double h, double width, double height)
+PolyMesh perturbedQuadRectangle(double h, double p, double width, double height)
+{
+    double x, y;
+    array<double, 2> pt;
+    vector<array<double, 2> > generatingPoints;
+    
+    for(x = 0.5*h; x <= width + kEPS; x += h)
+    {
+        for(y = 0.5*h; y <= height + kEPS; y += h)
+        {
+            pt[0] = x + h*p*double(rand() - RAND_MAX/2)/RAND_MAX;
+            pt[1] = y + h*p*double(rand() - RAND_MAX/2)/RAND_MAX;
+            generatingPoints.push_back(pt);
+        }
+    }
+    
+    return PolyMesh(generatingPoints, width, height);
+}
+
+PolyMesh perturbedTriRectangle(double h, double p, double width, double height)
 {
     double x, y;
     array<double, 2> pt;
     vector<array<double, 2> > generatingPoints;
     PolyMesh msh;
     
-    for(x = 0.5*h; x <= width; x += h)
+    for(x = 0.5*h; x <= width + kEPS; x += h)
     {
-        for(y = 0.5*h; y <= height; y += h)
+        for(y = 0.5*h; y <= height + kEPS; y += h)
         {
             pt[0] = x;
             pt[1] = y;
+            
+            if (x > 0.5*h && x < width - h && y > 0.5*h && y < height - h)
+            {
+                pt[0] += h*p*double(rand() - RAND_MAX/2)/RAND_MAX;
+                pt[1] += h*p*double(rand() - RAND_MAX/2)/RAND_MAX;
+            }
+            
             generatingPoints.push_back(pt);
         }
     }
     
     msh.triangulate(generatingPoints);
+    return msh;
+}
+
+PolyMesh triRectangle(double h, double width, double height)
+{
+    double x, y;
+    array<double, 2> pt;
+    vector<array<double, 2> > generatingPoints;
+    PolyMesh msh;
+    vector<int> polygon;
+    int idx1, idx2, idx3, idx4;
+    
+    for(x = 0; x + h <= width + kEPS; x += h)
+    {
+        for(y = 0; y + h <= height + kEPS; y += h)
+        {
+            pt[0] = x;
+            pt[1] = y;
+            idx1 = msh.addVertex(pt);
+            
+            pt[0] = x+h;
+            pt[1] = y;
+            idx2 = msh.addVertex(pt);
+            
+            pt[0] = x+h;
+            pt[1] = y+h;
+            idx3 = msh.addVertex(pt);
+            
+            pt[0] = x;
+            pt[1] = y+h;
+            idx4 = msh.addVertex(pt);
+            
+            polygon = {idx1, idx2, idx3};
+            msh.p.push_back(polygon);
+            polygon = {idx1, idx3, idx4};
+            msh.p.push_back(polygon);
+        }
+    }
+    
+    msh.np = msh.p.size();
+    msh.computep2p();
+    msh.computebb();
+    msh.computeTriangulation();
+    
+    //msh.triangulate(generatingPoints);
     
     return msh;
 }
--- src/Meshes.h
+++ src/Meshes.h
@@ -6,6 +6,8 @@
 
 // tile (width)x(height) rectangle by quadrilaterals, hexagons, or triangles
 PolyMesh quadRectangle(double h, double width, double height);
+PolyMesh perturbedQuadRectangle(double h, double p, double width, double height);
+PolyMesh perturbedTriRectangle(double h, double p, double width, double height);
 PolyMesh hexRectangle(double h, double width, double height);
 PolyMesh triRectangle(double h, double width, double height);
 PolyMesh honeycombRectangle(double h, double width, double height);
