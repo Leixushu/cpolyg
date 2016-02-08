2bd037d8c4cc5a12456bffc4e8d87b4e0318e75c
will-e-p <will.e.p@gmail.com>
2016-02-08 13:29:32 -0800

    Slightly reworked Block Jacobi preconditioner, added comments

--- src/Preconditioners.cpp
+++ src/Preconditioners.cpp
@@ -7,30 +7,24 @@ using namespace arma;
 void BlockJacobi::solve(double *x)
 {
     int i;
-    double *A = new double[bl*bl];
-    int *ipvt = new int[bl];
-    
     // do a block diagonal solve, overwrite the vector x
     for (i = 0; i < n; i++)
     {
-        copy(blocks[i].memptr(), blocks[i].memptr() + bl*bl, A);
-        // LAPACK LU factorization
-        cdgetrf(bl, bl, A, bl, ipvt);
-        // LU dense solve
-        cdgetrs('N', bl, 1, A, bl, ipvt, x + i*bl, bl);
+        // Solve using the LU factorization of the diagonal blocks
+        cdgetrs('N', bl, 1, factoredBlocks(i).memptr(), bl, pivots(i).memptr(), 
+                x + i*bl, bl);
     }
-    
-    delete[] A;
-    delete[] ipvt;
 }
 
 BlockJacobi::BlockJacobi(BlockMatrix &M)
 {
     int i, j, k;
-    blocks.resize(M.n_rows, zeros<mat>(M.bl, M.bl));
     
     n = M.n_rows;
     bl = M.bl;
+    
+    factoredBlocks.set_size(n);
+    pivots.set_size(n);
     
     // extract the diagonal blocks from M
     for (i = 0; i < n; i++)
@@ -40,7 +34,12 @@ BlockJacobi::BlockJacobi(BlockMatrix &M)
             j = M.colIndices[k];
             if (j == i)
             {
-                blocks[i] = M.blocks[k];
+                // allocate memory for the pivot indices
+                pivots(i).set_size(bl);
+                factoredBlocks(i) = M.blocks[k];
+                // LAPACK LU factorization
+                cdgetrf(bl, bl, factoredBlocks(i).memptr(), bl, pivots(i).memptr());
+                break;
             }
         }
     }
--- src/Preconditioners.h
+++ src/Preconditioners.h
@@ -1,33 +1,55 @@
 #pragma once
 
 #include <armadillo>
-#include <vector>
 #include "BlockMatrix.h"
 #include "Jacobian.h"
 
+// Provide various preconditioners for use in linear solvers
+
+// Abstract class for preconditioners
+struct Preconditioner
+{
+    // Perform solve Mx = b, overwriting input array
+    virtual void solve(double *x) = 0;
+};
+
+// This 'preconditioner' does nothing
 struct NoPreconditioner : Preconditioner
 {
     void solve(double *x) { };
 };
 
+// Block Jacobi preconditioner: M = diagonal blocks of A
 struct BlockJacobi : Preconditioner
 {
+    // number of block rows
     int n;
+    // size of each block
     int bl;
-    std::vector<arma::mat> blocks;
+    // store the LU factorization of each block (and pivot indices)
+    arma::field<arma::mat> factoredBlocks;
+    arma::field<arma::Col<int>> pivots;
     
+    // do the block diagonal solve
     void solve(double *x);
     
+    // extract the diagonal blocks and perform the factorization for each
     BlockJacobi(BlockMatrix &M);
 };
 
+// Block ILU(0) preconditioner: incomplete LU with same (block) sparsity as A
 struct BlockILU0 : Preconditioner
 {
+    // number of block rows
     int n;
+    // size of each block
     int bl;
     
+    // store the diagonal, L, and U matrices in block compressed sparse row format
     BlockMatrix DD, L, DU;
-    BlockILU0(Jacobian &A);
     
+    // do forward and then backward substitution: DU\(DD*(L\x))
     void solve(double *x);
+    // perform the factorization
+    BlockILU0(Jacobian &A);
 };
