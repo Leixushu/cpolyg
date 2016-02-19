5e789ecf1da2fb2ade94b2e85004cc1e62229100
will-e-p <will.e.p@gmail.com>
2016-02-19 14:17:07 -0800

    Moved ProductFunctor definition to MassMatrix.cpp

--- src/MassMatrix.cpp
+++ src/MassMatrix.cpp
@@ -7,6 +7,25 @@
 using namespace arma;
 using namespace std;
 
+// Use the product functor to integrate the product of basis functions
+struct ProductFunctor : FnFunctor
+{
+    arma::vec phi, psi;
+    int i;
+    int m;
+    PolyMesh &msh;
+
+    ProductFunctor(PolyMesh &a_msh) : msh(a_msh) {};
+
+    double operator()(double x, double y) const
+    {
+        double xx, yy;
+        msh.getLocalCoordinates(i, x, y, xx, yy);
+    
+        return Leg2D(xx, yy, m, phi)*Leg2D(xx, yy, m, psi);
+    };
+};
+
 // Allocate and compute mass matrix
 MassMatrix::MassMatrix(PolyMesh &m, int d) : msh(m), deg(d)
 {
--- src/MassMatrix.h
+++ src/MassMatrix.h
@@ -10,25 +10,6 @@
 // Implement the mass matrix as a special type of block matrix
 struct MassMatrix : BlockMatrix
 {
-    // Use the product functor to integrate the product of basis functions
-    struct ProductFunctor : FnFunctor
-    {
-        arma::vec phi, psi;
-        int i;
-        int m;
-        PolyMesh &msh;
-    
-        ProductFunctor(PolyMesh &a_msh) : msh(a_msh) {};
-    
-        double operator()(double x, double y) const
-        {
-            double xx, yy;
-            msh.getLocalCoordinates(i, x, y, xx, yy);
-        
-            return Leg2D(xx, yy, m, phi)*Leg2D(xx, yy, m, psi);
-        };
-    };
-    
     std::vector<arma::mat> LU;
     std::vector<arma::Col<int>> ipvt;
     
