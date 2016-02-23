29d55c199d71f52c39140eceee7069a768ce86fe
will-e-p <will.e.p@gmail.com>
2016-02-22 23:54:31 -0800

    Removed vector callbacks -- unused

--- src/Functors.h
+++ src/Functors.h
@@ -4,7 +4,6 @@
 #include <iostream>
 
 typedef double (*FnCallback)(double x, double y);
-typedef arma::vec (*VecCallback)(double x, double y);
 
 /// Abstract class for a functor that takes (x,y) coordinates and return a function value
 struct FnFunctor
@@ -35,6 +34,7 @@ struct VecFunctor
 {
     int n_rows = 1;
     int n_cols = 1;
+    // allow for time-dependency with this variable t
     double t = 0;
     
     //VecFunctor() { };
@@ -42,21 +42,6 @@ struct VecFunctor
     
     virtual arma::mat operator()(double x, double y) const = 0;
     virtual ~VecFunctor() {};
-};
-
-struct VecCallbackFunctor : VecFunctor
-{   
-    VecCallback cb;
-    
-    VecCallbackFunctor(VecCallback fn, int a_nc) : VecFunctor(a_nc), cb(fn)
-    {
-        n_rows = a_nc;
-    }
-    
-    arma::mat operator()(double x, double y) const
-    {
-        return cb(x, y);
-    }
 };
 
 struct VecFnFunctor : VecFunctor
