d250b86e1001764cd758d4b1d69db63cec7e97dd
will-e-p <will.e.p@gmail.com>
2016-03-29 17:53:05 -0400

    Modified IRK example

--- examples/irk.cpp
+++ examples/irk.cpp
@@ -19,11 +19,11 @@ int main(int argc, char ** argv)
 {
     using namespace std;
     // specify the degree of polynomials to use
-    int deg = 1;
+    int deg = 4;
     // specify the size of each element in the mesh
-    double h = 0.05;
+    double h = 0.1;
     // specify the time step
-    double dt = h*0.25;
+    double dt = h*0.5;
     
     // create the mesh, in this case hexagonal tessellation
     // of the unit square
@@ -41,37 +41,36 @@ int main(int argc, char ** argv)
     
     MeshFn f = MeshFn(msh, gaussian, deg);
     
-    int K = 200;
-    dt = M_PI/K;
+    int K = 2;
     
     DIRK3 tiDIRK(M, eqn);
-    /*f = MeshFn(msh, gaussian, deg);
+    f = MeshFn(msh, gaussian, deg);
     for (int i = 0; i < K; i++)
     {
         cout << "Beginning timestep " << i << endl;
         f = tiDIRK.advance(f, dt, i*dt);
-        f.gnuplot("plt/u" + to_string(i) + ".gnu");
-    }*/
+        f.gnuplot("plt/dirk" + to_string(i) + ".gnu");
+    }
     
     // create the time integration object
     IRK3 ti(M, eqn);
     
     // create a mesh function by interpolating the 
     // initial conditions defined by the function above
-//     f = MeshFn(msh, gaussian, deg);
-//     for (int i = 0; i < K; i++)
-//     {
-//         cout << "Beginning timestep " << i << endl;
-//         f = ti.advance(f, dt, i*dt);
-//         f.gnuplot("plt/u" + std::to_string(i) + ".gnu");
-//     }
+    f = MeshFn(msh, gaussian, deg);
+    for (int i = 0; i < K; i++)
+    {
+        cout << "Beginning timestep " << i << endl;
+        f = ti.advance(f, dt, i*dt);
+        f.gnuplot("plt/irk1" + std::to_string(i) + ".gnu");
+    }
     
     f = MeshFn(msh, gaussian, deg);
     for (int i = 0; i < K; i++)
     {
         cout << "Beginning timestep " << i << endl;
         f = ti.newAdvance(f, dt, i*dt);
-        f.gnuplot("plt/u" + to_string(i) + ".gnu");
+        f.gnuplot("plt/irk2" + to_string(i) + ".gnu");
     }
     
     return 0;
